c0ef93f4f419ff509d97592e5272cccd4497c4be
JailtonSampaio <jailton@jksolucoes.com.br>
2017-03-19 01:14:03 -0300

    Revert "tudo normal com com nome da app e photo no model user"
    This reverts commit 2fdfe7f9554363f010c7555b31afb3501065f706.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -86,16 +86,17 @@ RailsAdmin.config do |config|
       field  :email
       field  :password
       field  :password_confirmation
-      field :photo
 
 
     end
 
       edit do
+<<<<<<< HEAD
 
-        field :photo
-
-
+        field  :photo
+=======
+        
+>>>>>>> parent of 999f63d... Incluir feld :photo no model user
         field  :password
         field  :password_confirmation
 
@@ -109,7 +110,7 @@ RailsAdmin.config do |config|
     #mudando o nome da aplicação
       config.main_app_name = ["My Annotations", "*Minhas Anotações*"]
 
-
+    
 
 
 
