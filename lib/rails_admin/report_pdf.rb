750598d4c7d88c435dd58a735b2a1e0050f2a370
JailtonSampaio <jailton@jksolucoes.com.br>
2017-04-18 09:17:59 -0300

    concluido a formatação do prawn criado hearder footer boby como BOX para manter o posicionamento em todas as paginas e fora deles inclui o numero de pagina chamando o number_pages

--- lib/rails_admin/report_pdf.rb
+++ lib/rails_admin/report_pdf.rb
@@ -50,20 +50,45 @@ module RailsAdmin
               # Configurando Retorno
               ramdom_file_name = (0...8).map { (65 + rand(26)).chr }.join
               Prawn::Document.new(PDF_OPTIONS) do |pdf|
+
+                pdf.repeat :all do
+                  # header
+
+                  pdf.bounding_box [pdf.bounds.left, pdf.bounds.top], :width  => pdf.bounds.width do
+                    pdf.image "./public/logo.png", :align => :left, :width => 100
+                    pdf.fill_color "000000"
+                    pdf.text "Produtividade dos Usuários", :size => 16, :style => :bold, :align => :center
+                    pdf.move_down 10
+                    pdf.fill_color "666666"
+                    pdf.text "Período #{frm_date[:ini].strftime("%d/%m/%Y")} à #{frm_date[:fim].strftime("%d/%m/%Y")}", :size => 12, :style => :bold, :align => :right
+                    pdf.fill_color "000000"
+                    pdf.stroke_horizontal_rule
+                    pdf.move_down 10
+                  end
+
+
+                  # footer
+                  # Inclui em baixo da folha do lado direito a data e hora
+                  pdf.bounding_box [pdf.bounds.left, pdf.bounds.bottom + 10], :width  => pdf.bounds.width do
+                    #pdf.stroke_horizontal_rule
+                    #pdf.number_pages "Desenvolvido por JK Soluções - Gerado em: #{(Time.now).strftime("%d/%m/%Y as %H:%M")} - Pagina <page> de <total>", { :start_count_at => 0, :page_filter => :all, :align => :right, :size => 14 }
+                    #pag =  pdf.page_count
+                    pdf.text "Desenvolvido por JK Soluções - Gerado em: #{(Time.now).strftime("%d/%m/%Y as %H:%M")}", :size => 8, :align => :right
+                  end
+                  #pdf.number_pages "Gerado: #{(Time.now).strftime("%d/%m/%y as %H:%M")} - Página ", :start_count_at => 0, :page_filter => :all, :size => 8 , :at => [140, 7]
+
+
+                end
+
+
                 pdf.font "Helvetica"
-                pdf.fill_color "000000"
-                pdf.text "Produtividade dos Usuários", :size => 16, :style => :bold, :align => :center
-                pdf.move_down 10
-                pdf.fill_color "666666"
-                pdf.text "Periodo #{frm_date[:ini]} à #{frm_date[:fim]}", :size => 14, :style => :bold, :align => :right
-                pdf.stroke_horizontal_rule
-                pdf.move_down 20
+
 
               #variaveis de contagem de anotações e a soma dos caracteres das anotações e tabela
               anno_total = 0
               anno_caract = 0
               tabela_user = []
-              tabela_user << ["Nome","Anotações","caracteres"]
+              tabela_user << ["Nome dos Usuários","Anotações","Caracteres"]
 
 
               frm_users.each do |us|
@@ -90,10 +115,19 @@ module RailsAdmin
                 end
 
               end
-              #pdf.table [[Prawn::Table::Cell::Text.new( pdf, [0,0], :content => "Title", :align => :center, :inline_format => true, :size => 12)], tabela_user]
-              pdf.table(tabela_user, [0,0], :content => "Title",:inline_format => true, :size => 12)
-              pdf.render_file File.join(Rails.root, "public/pdfs", "#{ramdom_file_name}.pdf")
 
+              #body
+              pdf.bounding_box([pdf.bounds.left, pdf.bounds.top - 80], :width  => pdf.bounds.width, :height => pdf.bounds.height - 100) do
+                #pdf.table [[Prawn::Table::Cell::Text.new( pdf, [0,0], :content => "Title", :align => :center, :inline_format => true, :size => 12)], tabela_user]
+                pdf.table tabela_user, :row_colors => ["F0F0F0", "FFFFCC"], :position => :center,  :column_widths => [300, 60, 60], :cell_style => { :size => 10 }
+
+              end
+
+
+              #inserindo numero de paginas
+               pdf.number_pages "Página <page> de <total>", :at => [0, 10], :size => 10
+
+              pdf.render_file File.join(Rails.root, "public/pdfs", "#{ramdom_file_name}.pdf")
               File.open("#{Rails.root}/public/pdfs/#{ramdom_file_name}.pdf", 'r') do |f|
                 send_data f.read.force_encoding('BINARY'), :filename => "#{params[:periodo]}dias_#{Date.current}.pdf", :type => "application/pdf", :disposition => "attachment"
               end
